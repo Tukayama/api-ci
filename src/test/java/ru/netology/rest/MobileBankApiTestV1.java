19019396c0b3c49235839c50a742abfbf663ee58
Tukayama <ya.tumanov2009@yandex.ru>
2021-11-27 15:39:43 +0300

    init commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,7 +1,7 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.RestAssured.given;
 public class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
